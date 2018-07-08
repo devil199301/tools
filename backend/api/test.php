72c3549f295a97dc3b724ebf7ddfe2ad5d5b83cb
devil199301 <devil199301@gmail.com>
2018-07-08 22:40:35 +0800

    測試讀寫檔案

--- backend/api/test.php
+++ backend/api/test.php
@@ -1,13 +1,25 @@
 <?php
+// 開啟原始文件
+$myfile = fopen("webdictionary.css", "r") or die("Unable to open file!");
+// 輸出文件
+$outputFile = fopen("output.css", "w");
 
-$file = 'example.txt';
-$newfile = 'example.png';
-$dir = 'D:/MZ001-01/opt/ag';
-if (mkdir($dir, 0700)) {
-    echo "成功建立資料夾" . "<br>";
-} else {
-    echo "建立資料夾失敗" . "<br>";
-}
-if (!copy('D:/MZ001-01/slot/ag/补鱼王2代.png', $dir .'/'. $newfile)) {
-    echo "failed to copy $file...\n";
-}
+// 讀取原始文件
+$file = fread($myfile, filesize("webdictionary.css"));
+// 取代空格
+$file = preg_replace('/\s(?=)/', '', $file);
+// 搜尋字串
+$search = ['color:#404c51', 'h1,h2'];
+// 預期
+$replaceString = ['color:#fff', 'h2,h1'];
+// 更改
+$replace = preg_split ('({(.*?)})e', $file);
+
+// 顯示
+print_r ($replace);
+
+// 寫入輸出檔案
+fwrite($outputFile, $replace);
+
+// 關閉文件
+fclose($myfile, $outputFile);
